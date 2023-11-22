b3c5634dd48829ef128beb504385c06556393bd0
ProfSathishR <30977778+Professor-Sathish@users.noreply.github.com>
2023-11-22 22:51:51 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,1 +1,13 @@
+def shift_orbs(orbs, factor, constant, direction, transform_after):
+    #code here
+    return orbs
+
+def shift_left(orbs):
+    return None #return which - code here?
+def shift_right(orbs):
+    return None #return which - code here?
+
+def transform_orbs(orbs, factor, constant):
+    #code here
+    return None #return which - code here?
 
