a26a724cbae607459956dbd0bdab379e3228d53b
ProfSathishR <30977778+Professor-Sathish@users.noreply.github.com>
2023-11-23 12:23:11 +0530

    Update test_solution.py

--- test_solution.py
+++ test_solution.py
@@ -11,7 +11,7 @@ def test_shift_orbs_left_transform_after():
 
     result = shift_orbs(orbs, factor, constant, direction, transform_after)
 
-    assert result == [8, 14, 12, 6]
+    assert result == [13, 10, 21, 6]
 
 def test_shift_orbs_right_transform_before():
     orbs = [3, 5, 7, 9]
@@ -22,28 +22,4 @@ def test_shift_orbs_right_transform_before():
 
     result = shift_orbs(orbs, factor, constant, direction, transform_after)
 
-    assert result == [8, 12, 6, 14]
-
-def test_shift_orbs_invalid_direction():
-    orbs = [3, 5, 7, 9]
-    factor = 2
-    constant = 3
-    direction = 'InvalidDirection'
-    transform_after = True
-
-    try:
-        shift_orbs(orbs, factor, constant, direction, transform_after)
-    except ValueError as e:
-        assert str(e) == "Invalid direction. Please choose 'Left' or 'Right'."
-
-def test_shift_orbs_invalid_transform_after():
-    orbs = [3, 5, 7, 9]
-    factor = 2
-    constant = 3
-    direction = 'Left'
-    transform_after = 'InvalidValue'
-
-    try:
-        shift_orbs(orbs, factor, constant, direction, transform_after)
-    except ValueError as e:
-        assert str(e) == "Invalid transform_after value. Please use True or False."
+    assert result == [9, 3, 5, 7]
