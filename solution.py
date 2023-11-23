451720ce13a291ffd1b707d0cce7ebe2b317b7d1
ProfSathishR <30977778+Professor-Sathish@users.noreply.github.com>
2023-11-23 11:16:52 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,26 @@
 def shift_orbs(orbs, factor, constant, direction, transform_after):
-    #code here
+    if direction not in ['Left', 'Right']:
+        raise ValueError("Invalid direction. Please choose 'Left' or 'Right'.")
+
+    if transform_after not in [True, False]:
+        raise ValueError("Invalid transform_after value. Please use True or False.")
+
+    if direction == 'Left':
+        orbs = shift_left(orbs)
+    elif direction == 'Right':
+        orbs = shift_right(orbs)
+
+    if transform_after:
+        orbs = transform_orbs(orbs, factor, constant)
+
     return orbs
 
 def shift_left(orbs):
-    return None #return which - code here?
+    return #code here
+
 def shift_right(orbs):
-    return None #return which - code here?
+    return #code here
 
 def transform_orbs(orbs, factor, constant):
-    #code here
-    return None #return which - code here?
-
+    transformed_orbs = [x * factor + constant if i % 2 == 0 else x + constant for i, x in enumerate(orbs)]
+    return transformed_orbs
